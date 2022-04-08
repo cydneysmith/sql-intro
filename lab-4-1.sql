b1ca294090f2eae9fb525ccd7614db25beff050b
cydneysmith <cydney.smith@kellogg.northwestern.edu>
2022-04-08 21:18:27 +0000

    did lab 3.1 exercise

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -4,3 +4,10 @@
 -- 2935
 
 
+-- how do we get barry bonds? just see if we can get to barry in players table. SELECT * FROM players WHERE first_name = "Barry" and last_name = "Bonds";
+-- can see what stats looks like by saying SELECT * FROM players;
+
+SELECT sum(stats.hits)
+from players INNER JOIN stats ON players.id = stats.player_id
+WHERE players.first_name = "Barry" and players.last_name = "Bonds";
+
